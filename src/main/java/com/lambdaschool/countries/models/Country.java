4c4f6a54419d3bb01c5848e377e838eef814c9d7
Claudiuts <sarb.claudiu391@gmail.com>
2021-03-11 17:10:16 -0600

    created repository

--- src/main/java/com/lambdaschool/countries/models/Country.java
+++ src/main/java/com/lambdaschool/countries/models/Country.java
@@ -68,4 +68,16 @@ public class Country
     public void setMedianage(int medianage) {
         this.medianage = medianage;
     }
+
+    @Override
+    public String toString()
+    {
+        return "Country{" +
+                "countryid=" + countryid +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                ", landmasskm2=" + landmasskm2 +
+                ", medianage=" + medianage +
+                '}';
+    }
 }
